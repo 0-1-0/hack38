e118dcca08e03e496fa986bcf917ee1742735f08
Nickolay Yegorov <nickolay@yegorov.info>
2013-07-25 00:30:21 -0700

    sdf

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ mongo.connect(process.env.MONGOLAB_URI, {}, function(error, db){
           requestCollection.insert(doc, function(error, result){
             requestCollection.find({loc:{$near: doc.loc, $maxDistance: 1000000.0}}).toArray(
               function(err, fbfriends){
-                res.write(JSON.stringify(fbfriends));
+                res.write(JSON.stringify(fbfriends) + err);
               });  
           });
         }
